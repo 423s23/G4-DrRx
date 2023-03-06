f3146ee1bf469686475875810d2d4718fc38575c
Devan-Eastman-Pittam <62622298+Devan-Eastman-Pittam@users.noreply.github.com>
2023-03-06 08:07:36 -0700

    Update Check_Data.java
    isi

--- Project/JavaCode/Check_Data.java
+++ Project/JavaCode/Check_Data.java
@@ -14,49 +14,80 @@ public class Check_Data {
         return("Data Not Found");
     }
 
+    public static String Suggested_ISI(int ISI){
+        if(ISI<=7){
+            return ("Insomnia Result: No clinically significant insomnia\n");
+        }
+        else if((8<=ISI)&&(ISI<=14)){
+            return ("Insomnia Result: Subthreshold insomnia\n");
+        }
+        else if((15<=ISI)&&(ISI<=21)){
+            return ("Insomnia Result: Clinical insomnia (moderate severity)\n");
+        }
+        else if((21<=ISI)&&(ISI<=28)){
+            return ("Insomnia Result: Clinical insomnia (severe)\n");
+        }
+        return("Data Not Found");
+    }
+
     // line is sent through call function in Questionnaire_Application
     public static String[] main(String line){
 
-                // while line isn't null
-                if (line != ""){
-                    int PHQ_total = 0, GAD_total = 0, ISI_total = 0, ASRS_total = 0;
-                    String[] testArray = line.split(",");
-                    String first = testArray[0];
-                    String last = testArray[1];
-                    String DOB = testArray[2];
-                    // tests for PHQ-9 in format, should be there
-                    if (testArray[3].equals("PHQ-9")){
-                        for (int i = 4; i < 4+9; i++) PHQ_total += Integer.parseInt(testArray[i]);
-                    }
-                    // tests for GAD-7 in format, should be there
-                    if (testArray[13].equals("GAD-7")){
-                        for (int i = 14; i < 14+7; i++) GAD_total += Integer.parseInt(testArray[i]);
-                    }
-                    // If PHQ Question 9 is anything but 0, take CSS
-                    if (!(testArray[12].equals("0"))){
-                        // IF CSS 4,5,6 is positive, Crisis intervention indicated
-                        // If no to 1,2,6 on CSS, move to other testing
-                        if (!(testArray[37].equals("0"))&&(testArray[38].equals("0"))&&(testArray[43].equals("0"))) {
-                            //REFER CSS SCREENING HERE. THREAT DETECTED
-                            //Maybe open new window to make sure the CSS positive is seen
-                        }
-                    }
+        // while line isn't null
+        if (line != ""){
+            int PHQ_total = 0, GAD_total = 0, ISI_total = 0, ASRS_total = 0;
+            String[] testArray = line.split(",");
+            String first = testArray[0];
+            String last = testArray[1];
+            String DOB = testArray[2];
+            // tests for PHQ-9 in format, should be there
+            if (testArray[3].equals("PHQ-9")){
+                for (int i = 4; i < 4+9; i++) PHQ_total += Integer.parseInt(testArray[i]);
+            }
+            // tests for GAD-7 in format, should be there
+            if (testArray[13].equals("GAD-7")){
+                for (int i = 14; i < 14+7; i++) GAD_total += Integer.parseInt(testArray[i]);
+            }
 
-                    if (testArray[29].equals("ASRS")){
-                        for (int i = 30; i < 30+6; i++) ASRS_total += Integer.parseInt(testArray[i]);
-                    }
+            // ISI TEST
+            if (testArray[21].equals("ISI")){
+                ISI_total += Integer.parseInt(testArray[22]);
+                ISI_total += Integer.parseInt(testArray[23]);
+                ISI_total += Integer.parseInt(testArray[24]);
+                ISI_total += Integer.parseInt(testArray[25]);
+                ISI_total += Integer.parseInt(testArray[26]);
+                ISI_total += Integer.parseInt(testArray[27]);
+                ISI_total += Integer.parseInt(testArray[28]);
+            }
 
-                    // End of Current Testing
-                    // This string array will have to be modified to include more tests
-                    // Suggested method will have to be modified to take all data, and return right recommendation
-                    String result[] = new String[5];
-                    result[0] = ("Patient: " + first + " " + last);
-                    result[1] = ("PHQ-9 Results: " + PHQ_total);
-                    result[2] = ("GAD-7 Results: " + GAD_total);
-                    result[3] = Suggested(PHQ_total,GAD_total);
-                    result[4] = ("ASRS Results: " + ASRS_total);
-                    return(result);
+            // If PHQ Question 9 is anything but 0, take CSS
+            if (!(testArray[12].equals("0"))){
+                // IF CSS 4,5,6 is positive, Crisis intervention indicated
+                // If no to 1,2,6 on CSS, move to other testing
+                if (!(testArray[37].equals("0"))&&(testArray[38].equals("0"))&&(testArray[43].equals("0"))) {
+                    //REFER CSS SCREENING HERE. THREAT DETECTED
+                    //Maybe open new window to make sure the CSS positive is seen
                 }
+            }
+
+            if (testArray[29].equals("ASRS")){
+                for (int i = 30; i < 30+6; i++) ASRS_total += Integer.parseInt(testArray[i]);
+            }
+
+            // End of Current Testing
+            // This string array will have to be modified to include more tests
+            // Suggested method will have to be modified to take all data, and return right recommendation
+            String result[] = new String[7];
+            result[0] = ("Patient: " + first + " " + last);
+            result[1] = ("PHQ-9 Results: " + PHQ_total);
+            result[2] = ("GAD-7 Results: " + GAD_total);
+            result[3] = ("ISI Results: "   + ISI_total);
+            result[4] = ("ASRS Results: "  + ASRS_total);
+            result[5] = Suggested(PHQ_total,GAD_total);
+            result[6] = Suggested_ISI(ISI_total);
+            //System.out.println(result[0].toString()+"\n"+result[1]+"\n"+result[2]+"\n"+result[3]+"\n"+result[4]+"\n"+result[5]+"\n"+result[6]);
+            return(result);
+        }
 
         return new String[0];
     }
