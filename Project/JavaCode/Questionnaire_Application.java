16f9ef84fabdd20e3d4fff422dc89da36cd4cc01
Devan-Eastman-Pittam <62622298+Devan-Eastman-Pittam@users.noreply.github.com>
2023-03-06 08:06:41 -0700

    Update Questionnaire_Application.java
    isi

--- Project/JavaCode/Questionnaire_Application.java
+++ Project/JavaCode/Questionnaire_Application.java
@@ -16,7 +16,7 @@ import java.util.Scanner;
 public class Questionnaire_Application extends JFrame {
 
 
-	public Questionnaire_Application() {
+    public Questionnaire_Application() {
         search_bar();
 
         // Below code commented out in case we need to add additional functionalities/windows to the application
@@ -29,16 +29,16 @@ public class Questionnaire_Application extends JFrame {
         JPanel next_button_panel = new JPanel();
 
         //------------------------------------------------//
-        
+
         JLabel jlabel = new JLabel("Doctor Assisting Application",SwingConstants.CENTER);
         jlabel.setFont(new Font("Verdana", Font.BOLD,26));
-        
+
         //jlabel.setForeground(Color.RED); Text Color Option
-        
+
         JButton option1 = new JButton("Patient Search Bar");
-        
+
         //------------------------------------------------//
-        
+
         option1.addActionListener(new ActionListener(){
             @Override
             public void actionPerformed(ActionEvent evt){search_bar();}
@@ -54,42 +54,52 @@ public class Questionnaire_Application extends JFrame {
         frame.add(button_panel);
         frame.add(next_button_panel);
 
-        frame.setExtendedState(JFrame.MAXIMIZED_BOTH); 
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  */
-	}
-	
+    }
+
     private void search_bar() { //write down personal info of patient
-    	JFrame f = new JFrame("Patient Lookup");
+        JFrame f = new JFrame("Patient Lookup");
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    	JPanel panel = new JPanel();
-    	panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
-    	
-    	JPanel wrapper = new JPanel( new FlowLayout(0, 0, FlowLayout.LEADING) );
-    	
-    	JTextField search_bar = new JTextField(30);
-    	JLabel jlabel = new JLabel("Enter Patient Name Below For Results:",SwingConstants.CENTER);
-    	JButton search_button = new JButton("Search");
-    	
-    	jlabel.setFont(new Font("Verdana", Font.BOLD,26));
-    	search_bar.setFont(new Font("Verdana", Font.BOLD,26));
-    	
-    	search_bar.setHorizontalAlignment(JTextField.CENTER);
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+
+        JPanel wrapper = new JPanel( new FlowLayout(0, 0, FlowLayout.LEADING) );
+
+        JTextField search_bar = new JTextField(30);
+        JLabel jlabel = new JLabel("Enter Patient Name Below For Results:",SwingConstants.CENTER);
+        JButton search_button = new JButton("Search");
+
+        jlabel.setFont(new Font("Verdana", Font.BOLD,26));
+        search_bar.setFont(new Font("Verdana", Font.BOLD,26));
+
+        search_bar.setHorizontalAlignment(JTextField.CENTER);
 
         JLabel jlabel2 = new JLabel("",SwingConstants.CENTER);
         jlabel2.setFont(new Font("Verdana", Font.BOLD,20));
+
         JLabel jlabel3 = new JLabel("",SwingConstants.CENTER);
         jlabel3.setFont(new Font("Verdana", Font.BOLD,20));
+
         JLabel jlabel4 = new JLabel("",SwingConstants.CENTER);
         jlabel4.setFont(new Font("Verdana", Font.BOLD,20));
+
         JLabel jlabel5 = new JLabel("",SwingConstants.CENTER);
         jlabel5.setFont(new Font("Verdana", Font.BOLD,20));
+
         JLabel jlabel6 = new JLabel("",SwingConstants.CENTER);
         jlabel6.setFont(new Font("Verdana", Font.BOLD,20));
-    	search_button.addActionListener(new ActionListener(){
+
+        JLabel jlabel7 = new JLabel("",SwingConstants.CENTER);
+        jlabel7.setFont(new Font("Verdana", Font.BOLD,20));
+
+        JLabel jlabel8 = new JLabel("",SwingConstants.CENTER);
+        jlabel8.setFont(new Font("Verdana", Font.BOLD,20));
+        search_button.addActionListener(new ActionListener(){
             @Override
             public void actionPerformed(ActionEvent evt){
-            	String patient_name = search_bar.getText();
+                String patient_name = search_bar.getText();
                 try {
                     String line;
                     Scanner sc = new Scanner(new File("Project\\testData\\sampleinput.csv"));
@@ -101,11 +111,15 @@ public class Questionnaire_Application extends JFrame {
                             //System.out.println(line);
                             Check_Data data = new Check_Data();
                             String[] result = data.main(line);
-                            jlabel2.setText(result[0].toString());
+
+                            jlabel2.setText(result[0].toString()); //assign text to label
                             jlabel3.setText(result[1].toString());
                             jlabel4.setText(result[2].toString());
                             jlabel5.setText(result[3].toString());
                             jlabel6.setText(result[4].toString());
+                            jlabel7.setText(result[5].toString());
+                            jlabel8.setText(result[6].toString());
+
                             break;
                         }
                     }
@@ -113,26 +127,29 @@ public class Questionnaire_Application extends JFrame {
                 } catch (FileNotFoundException e) {
                     // ignore
                 }
-            	//after finding it you should probably open a new panel for displaying the data
+                //after finding it you should probably open a new panel for displaying the data
             }
-            
-    	});
-    	
-    	wrapper.add(search_bar);
-    	wrapper.add(search_button);
-    	
-    	panel.add(jlabel);//adds text block to text displaying panel
-    	panel.add(wrapper);
-        // labels for patient recommendation
+
+        });
+
+        wrapper.add(search_bar);
+        wrapper.add(search_button);
+
+        panel.add(jlabel);//adds text block to text displaying panel
+        panel.add(wrapper);
+
+        // labels for patient recommendation, have to be added to be able to display
         panel.add(jlabel2);
         panel.add(jlabel3);
         panel.add(jlabel4);
-        panel.add(jlabel6);
-        panel.add(jlabel5);     // Recommendation label
-    	
-    	f.add(panel); //adds the text displaying panel to the frame
-    	
-    	f.setExtendedState(JFrame.MAXIMIZED_BOTH); 
+        panel.add(jlabel5);
+        panel.add(jlabel6); // Recommendation label
+        panel.add(jlabel7);
+        panel.add(jlabel8);
+
+        f.add(panel); //adds the text displaying panel to the frame
+
+        f.setExtendedState(JFrame.MAXIMIZED_BOTH);
         f.setVisible(true);
     }
     @SuppressWarnings("unused")
