f40e3371c345f82127fdbb36db3831820b38ac93
Aurora Duskin <auroraduskin@gmail.com>
2023-04-14 09:53:27 -0600

    Added Testing

---
+++ Test/Check_DataTest.java
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Check_DataTest {
+
+    @Test
+    void suggested() {
+        assertTrue(true);
+    }
+
+    @Test
+    void suggested_ISI() {
+        assertTrue(true);
+    }
+
+    @Test
+    void clean() {
+        assertTrue(true);
+    }
+
+    @Test
+    void main() {
+        assertTrue(true);
+    }
+}
---
+++ Test/Questionnaire_ApplicationTest.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Questionnaire_ApplicationTest {
+
+    @Test
+    void main() {
+        assertTrue(true);
+    }
+
+    @Test
+    void display() {
+        assertTrue(true);
+    }
+}
